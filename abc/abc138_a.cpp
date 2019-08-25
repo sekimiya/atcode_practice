e3dc4edd16844d4b99d71e7bca85ad3c5411ff15
yuuta seki <syakure9@gmail.com>
2019-08-26 01:16:30 +0900

    ac abc138_a

---
+++ abc/abc138_a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    string s;
+    cin >> a >> s;    
+    if(a < 3200){
+        cout << "red" << endl;
+    }
+    else if(a>=3200){
+        cout << s << endl;
+    }
+    
+}
