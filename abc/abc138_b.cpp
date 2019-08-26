a496ce652260c54417fa7c7d8d10db7031492e89
yuuta seki <syakure9@gmail.com>
2019-08-27 01:48:00 +0900

    ac abc138_b

--- abc/abc138_b.cpp
+++ abc/abc138_b.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector> 
 using namespace std;
 
 int main(){
+	vector<double> a; // <T>でT型のvector
 
-    int N;
-    vector<double> A;
+	int n = 2;
+	cin >> n;
+
+	while(n--) {
+		int x;
+		cin >> x;
+		a.push_back(x);
+	}
     
+    double recip;
+
+    for(int i=0; i<a.size(); i++){
+        recip  += 1/a.at(i);
+    }
+
+    double f_recip;
+
+    f_recip = 1/recip;
+
+    cout<< f_recip <<endl;
+
 }
