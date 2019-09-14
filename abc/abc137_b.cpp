7d710877196191612d9a16ad113dca60f699bcda
yuuta seki <syakure9@gmail.com>
2019-09-15 03:17:25 +0900

    ac abc137_b

--- abc/abc137_b.cpp
+++ abc/abc137_b.cpp
@@ -9,27 +9,35 @@ int main(){
     int K = 3; //黒く塗られたK個の石
     int X = 7; //黒い石の座標
 
-    // cin >> K >> X;
+    cin >> K >> X;
 
     //+に長い
-    string right_stone;
+    // string right_stone;
+
+    vector<int> ans;
 
     int right_pos;
 
     for(int i=X; i<=X+K-1; i++){
-        right_stone += to_string(i);
+        ans.push_back(i);
     }
 
     //-に長い
-    string left_stone;
+    // string left_stone;
     
     for(int i=X; i>=X-K+1; i--){
-        left_stone += to_string(i);
+        ans.push_back(i);
     }
 
-    string stone_pos = right_stone + left_stone;
-    vector<int> f_stone_pos;
+    // sort(ans.begin(), ans.end());
+    // unique(ans.begin(), ans.end());
 
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+    for(int i=0; i<ans.size(); i++) {
+        cout << ans.at(i) << " ";
+    }
     //todo:重複文字を削除してsortしてintへ変換して出力
 
     return 0;
