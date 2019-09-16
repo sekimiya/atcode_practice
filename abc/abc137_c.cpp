cfa4b76d592665749f70537663f34db66b3fdde2
yuuta seki <syakure9@gmail.com>
2019-09-16 21:16:56 +0900

    tle abc137_c

---
+++ abc/abc137_c.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector> 
+#include <algorithm> 
+
+using namespace std;
+int main(){
+    // long N = 3;
+    // vector<string> v = {"acornistnt", "peanutbomb", "constraint"};
+    
+    // long N =2;
+    // vector<string> v = {"oneplustwo" ,"ninemodsix"};
+    // // int N = 5;
+    // vector<string> v = {
+    // "abaaaaaaaa"
+    // ,"oneplustwo"
+    // ,"aaaaaaaaba"
+    // ,"twoplusone"
+    // ,"aaaabaaaaa"
+    // };
+
+    
+    long N;
+    vector<string> v;
+
+    cin >> N;
+
+	for(long i=0; i<N; i++) {
+		string x;
+		cin >> x;   
+        v.push_back(x);
+        sort(v[i].begin(),v[i].end());
+    //     sort(v[i].begin(),v[i].end());
+	};
+
+
+    // for(long i=0; i<N; i++){
+    //     sort(v[i].begin(),v[i].end());
+    //     // cout << v[i] << endl;
+    // }
+    // for(long i=0; i<N; i++){
+    //     sort(v[i].begin(),v[i].end());
+    //     // cout << v[i] << endl;
+    // }
+
+    long ans_n = 0;
+
+    // for(long i=0; i<N; i++){
+    //     if (std::equal(v.begin(), v.end(), v.begin())) {
+    //         std::cout << "equal" << std::endl;
+    //         ans_n++;
+    //     } else {
+    //         std::cout << "not equal" << std::endl;
+    //     }
+    // }
+
+
+
+    for(long i=0; i<N; i++){
+        string tmp_i = v[i];
+        for(long j=i+1; j<N; j++){
+            string tmp_j = v[j];
+            // if(i!=j && j!=i  && tmp_i == tmp_j)
+            if(tmp_i == tmp_j){
+                    ans_n ++;
+                    // cout << "i" << "" << i << endl;
+                    // cout << "j" << "" << j << endl;
+            }
+        }
+    }
+    
+    cout << ans_n << endl;
+
+	// for(int i=0; i<N; i++) {
+	// 	string x;
+	// 	cin >> x;   
+    //     v.push_back(x);
+	// };
+
+}
