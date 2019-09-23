98c7ee503b841ef30e355be7b6a74c8a3a4d6371
yuuta seki <syakure9@gmail.com>
2019-09-23 23:59:06 +0900

    ac 140_a

---
+++ abc/abc140_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector> 
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int N = 9;
+    cin >> N;
+    
+    int ans = N*N*N;
+    
+    cout << ans <<endl;
+}
